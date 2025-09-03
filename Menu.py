c8ec970257c85f66135d1a1d0e57311a2bf3909c
camilogamerytty <camiloarevaloroa4@gmail.com>
2025-09-03 12:39:43 -0500

    plantilla

---
+++ Menu.py
@@ -0,0 +1,13 @@
+import math
+
+def calculadora():
+  while True:
+print ("/n ==== calculadora de phyton")
+print ("Suma")
+print ("Resta")
+print ("Multiplicación")
+print ("Division")
+print ("raiz cuadrada")
+print ("porcentaje")
+opcion = input ("elige una opcion Suma 1, Resta 2, multiplicacion 3, Divicion 4, raiz cuadrada 5, porcentaje 6")
+
